338f67c825adcf14d1cbb04b21c0e974eb33ec17
katamus <christian.catamuscay.h@gmail.com>
2021-11-02 21:05:55 -0500

    ejemplo de uso de un viewChild para acceder a un componente tanto para su propiedad como sus atributos públicos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { HijoComponent } from './hijo/hijo.component';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,7 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 export class AppComponent {
   title = 'ejemplosCore';
   @ViewChild('inputNombre',{static:true}) inputNombre!:ElementRef;
-
+  @ViewChild(HijoComponent,{static:true}) hijo!:HijoComponent
   constructor(){
     
   }
@@ -18,5 +19,6 @@ export class AppComponent {
     //this.inputNombre.nativeElement.style.background = "red";
     //this.inputNombre.nativeElement.focus();
     this.inputNombre.nativeElement.select();
+    this.hijo.titulo = "lo modifique desde el padre"
   }
 }
