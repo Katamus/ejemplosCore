8cc842f7710cd9e978a741b358601ac47b572ae1
katamus <christian.catamuscay.h@gmail.com>
2021-11-02 20:57:57 -0500

    Ejemplo de uso del viewChild de angular, definicion de variable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ejemplosCore';
+  @ViewChild('inputNombre',{static:true}) inputNombre!:ElementRef;
+
+  constructor(){
+    
+  }
+
+  mostrar(){
+    console.log(this.inputNombre.nativeElement.value);
+    //this.inputNombre.nativeElement.style.background = "red";
+    //this.inputNombre.nativeElement.focus();
+    this.inputNombre.nativeElement.select();
+  }
 }
